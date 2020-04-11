b8575495514040e4703746f91138c5e50f11f173
caiwenglong <836472117@qq.com>
2020-04-11 20:34:24 +0800

    first commit

--- vue.config.js
+++ vue.config.js
@@ -5,14 +5,17 @@ function resolve (dir) {
 }
 
 const BASE_URL = process.env.NODE_ENV === 'production' ? '/' : '/';
-
 module.exports = {
   lintOnSave: false,
   publicPath: BASE_URL, // 在vue-cli.3.3版本后 baseUrl被废除了，因此这边要写成 publicPath
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src'))
-      .set('@_c').resolve('src/components');
+      .set('@img', resolve('src/assets/img'))
+      .set('@styl', resolve('src/assets/styl'))
+      .set('@js', resolve('src/assets/js'))
+      .set('@cp', resolve('src/components'))
+      .end();
   },
   productionSourceMap: false,
   devServer: {
